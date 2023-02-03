06dbf6958c5d9083fbb12c397664a901c090f39b
niraj <nirajbnd@gmail.com>
2023-02-03 17:57:24 +0530

    added mock for feign client

--- src/main/java/com/nirajtailor/feignintegration/Resource/FeignTestController.java
+++ src/main/java/com/nirajtailor/feignintegration/Resource/FeignTestController.java
@@ -1,18 +1,13 @@
 package com.nirajtailor.feignintegration.Resource;
 
 import com.nirajtailor.feignintegration.Model.Response;
-import com.nirajtailor.feignintegration.Model.Student;
-import com.nirajtailor.feignintegration.Model.User;
 import com.nirajtailor.feignintegration.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("user")
--- src/test/java/com/nirajtailor/feignintegration/FeignIntegrationApplicationTests.java
+++ src/test/java/com/nirajtailor/feignintegration/FeignIntegrationApplicationTests.java
@@ -1,13 +1,47 @@
 package com.nirajtailor.feignintegration;
 
+import com.github.tomakehurst.wiremock.client.ResponseDefinitionBuilder;
+import com.github.tomakehurst.wiremock.client.WireMock;
+import com.nirajtailor.feignintegration.Model.User;
+import com.nirajtailor.feignintegration.Proxy.UserProxy;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import wiremock.org.apache.commons.io.IOUtils;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+@ActiveProfiles("test")
 @SpringBootTest
-class FeignIntegrationApplicationTests {
+@AutoConfigureWireMock(port = 8011)
+public class FeignIntegrationApplicationTests {
 
+	@Autowired
+	private UserProxy userProxy;
 	@Test
-	void contextLoads() {
+	public void getAllUsers() throws Exception {
+
+		WireMock.stubFor(
+				WireMock.get(WireMock.urlEqualTo("/user/getUsers"))
+						.willReturn(new ResponseDefinitionBuilder()
+								.withHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE)
+								.withStatus(HttpStatus.OK.value())
+								.withBody(read("stubs/users.json"))));
+
+		List<User> response = userProxy.getUsers(null, null, null);
+
+		System.out.println(response);
+	}
+
+	private String read(String location) throws IOException {
+		return IOUtils.toString(new ClassPathResource(location).getInputStream(), StandardCharsets.UTF_8);
 	}
 
 }
